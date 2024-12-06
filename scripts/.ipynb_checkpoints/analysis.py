40f53f86bbf00e7956a3d0c8397836561917aa33
LittleLion930 <masudal@uci.edu>
2024-12-06 19:18:11 +0000

    Creates a 2D array of latitude and longittude coordinates

--- scripts/.ipynb_checkpoints/analysis.py
+++ scripts/.ipynb_checkpoints/analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA 
-# from scipy.spatial import distance
+from scipy.spatial import distance
 
 # Loads time series data into a Numpy array
 data = np.array(pd.read_csv('/home/masudal/EcoSense/data/enviornmental_data.csv')['temperature'])
@@ -12,3 +12,12 @@ results = model.fit()
 
 # Makes predictions
 forecast = results.forecast(steps=30)
+
+# Creates a 2D array of latitude and longtitude coordinates
+lat1 = 33.6846  # Example latitude for Irvine
+lon1 = 117.8265 # Example longtitude for Irvine
+lat2 = 34.0522 # Example latitude for Los Angeles
+lon2 = -118.2437 # Example longtitude for Los Angeles
+coordinates = np.array([[lat1, lon1], [lat2, lon2]])
+
+# Calculate distances between all points
