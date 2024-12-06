1877815f7d65ef65468ee8e0dd57f588afbb9a5a
LittleLion930 <masudal@uci.edu>
2024-12-06 19:10:42 +0000

    Makes predictions

--- scripts/.ipynb_checkpoints/analysis.py
+++ scripts/.ipynb_checkpoints/analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA 
+# from scipy.spatial import distance
 
 # Loads time series data into a Numpy array
 data = np.array(pd.read_csv('/home/masudal/EcoSense/data/enviornmental_data.csv')['temperature'])
@@ -8,3 +9,6 @@ data = np.array(pd.read_csv('/home/masudal/EcoSense/data/enviornmental_data.csv'
 # Fit ARIMA model
 model = ARIMA(data, order=(1, 1, 1))
 results = model.fit()
+
+# Makes predictions
+forecast = results.forecast(steps=30)
