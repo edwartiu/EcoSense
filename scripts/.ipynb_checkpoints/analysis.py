7105648928831f4f43f60ddfc057ae99079ea0f2
LittleLion930 <masudal@uci.edu>
2024-12-06 19:00:04 +0000

    Resolved merge conflicts again

--- scripts/.ipynb_checkpoints/analysis.py
+++ scripts/.ipynb_checkpoints/analysis.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
-# from statsmodels.tsa.arima.model import ARIMA 
+from statsmodels.tsa.arima.model import ARIMA 
 
 # Loads time series data into a Numpy array
 data = np.array(pd.read_csv('/home/masudal/EcoSense/data/enviornmental_data.csv')['temperature'])
+
+# Fit ARIMA model
+model = ARIMA(data, order=(1, 1, 1))
+results = model.fit()
